ff1e54ce1102df41f96b7fe5ff67fc7ab75e9f4c
bryanyee33 <65088428+bryanyee33@users.noreply.github.com>
2024-12-10 10:37:19 +0800

    Implement manual KSU hooks

--- fs/exec.c
+++ fs/exec.c
@@ -1875,6 +1875,12 @@ out_files:
 	return retval;
 }
 
+#ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+#endif
+
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1882,6 +1888,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 {
 	struct linux_binprm *bprm;
 	int retval;
+
+	#ifdef CONFIG_KSU
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+        #endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
