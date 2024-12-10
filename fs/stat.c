4df1ae0bc68d5aaa51cc48a042106c6efe1b56e9
bryanyee33 <65088428+bryanyee33@users.noreply.github.com>
2024-12-10 16:23:26 +0800

    Revert: Implement manual KSU hooks

--- fs/stat.c
+++ fs/stat.c
@@ -156,10 +156,6 @@ int vfs_fstat(int fd, struct kstat *stat)
 	return error;
 }
 
-#ifdef CONFIG_KSU
-extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
-#endif
-
 /**
  * vfs_statx - Get basic and extra attributes by filename
  * @dfd: A file descriptor representing the base dir for a relative filename
@@ -182,10 +178,6 @@ static int vfs_statx(int dfd, const char __user *filename, int flags,
 	unsigned lookup_flags = 0;
 	int error;
 
-        #ifdef CONFIG_KSU
-	ksu_handle_stat(&dfd, &filename, &flags);
-        #endif
-	
 	if (flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT | AT_EMPTY_PATH |
 		      AT_STATX_SYNC_TYPE))
 		return -EINVAL;
