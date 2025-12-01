1b1dcafc106f062e528b6309162ee828efc263fa
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-12-01 20:56:24 +0530

    build.sh: Rework build script to allow build separation
    Allow building kernel image, dtbs and modules separately to save
    time during testing.

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #
 # Compile script for Xiaomi 8450 kernel, dts and modules with AOSPA
-# Copyright (C) 2024 Adithya R.
+# Copyright (C) 2024-2025 Adithya R.
 
 SECONDS=0 # start builtin bash timer
 KP_ROOT="$(realpath ../..)"
-SRC_ROOT="$HOME/pa"
 TC_DIR="$KP_ROOT/prebuilts-master/clang/host/linux-x86/clang-r510928"
 PREBUILTS_DIR="$KP_ROOT/prebuilts/kernel-build-tools/linux-x86"
 BRANCH="$(git branch --show-current)"
@@ -15,24 +14,22 @@ DT_REPO="sm8450-devicetrees"
 DO_CLEAN=false
 NO_LTO=false
 ONLY_CONFIG=false
+ONLY_KERNEL=false
+ONLY_DTB=false
+ONLY_MODULES=false
 TARGET=
 DTB_WILDCARD="*"
 DTBO_WILDCARD="*"
 
-while [ "${#}" -gt 0 ]; do
-    case "${1}" in
-        -c | --clean )
-                DO_CLEAN=true
-                ;;
-        -n | --no-lto )
-                NO_LTO=true
-                ;;
-        -o | --only-config )
-                ONLY_CONFIG=true
-                ;;
-        * )
-                TARGET="${1}"
-                ;;
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -c | --clean) DO_CLEAN=true ;;
+        -n | --no-lto) NO_LTO=true ;;
+        -o | --only-config) ONLY_CONFIG=true ;;
+        -k | --only-kernel) ONLY_KERNEL=true ;;
+        -d | --only-dtb) ONLY_DTB=true ;;
+        -m | --only-modules) ONLY_MODULES=true ;;
+        *) TARGET="$1" ;;
     esac
     shift
 done
@@ -59,13 +56,6 @@ DTB_COPY_TO="$KERNEL_DIR/dtbs"
 DTBO_COPY_TO="$DTB_COPY_TO/dtbo.img"
 VBOOT_DIR="$KERNEL_DIR/vendor_ramdisk"
 VDLKM_DIR="$KERNEL_DIR/vendor_dlkm"
-
-# AK3_DIR="$HOME/AnyKernel3"
-# ZIPNAME="aospa-kernel-$TARGET-$(date '+%Y%m%d-%H%M').zip"
-# if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
-#    head=$(git rev-parse --verify HEAD 2>/dev/null); then
-#     ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
-# fi
 
 DEFCONFIG="gki_defconfig"
 DEFCONFIGS="vendor/waipio_GKI.config \
@@ -102,16 +92,17 @@ case "$TARGET" in
         ;;
 esac
 
-export PATH="$TC_DIR/bin:$PREBUILTS_DIR/bin:$PATH"
+##
+## Helper functions
+##
 
-function m() {
-    make -j$(nproc --all) O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 \
-        DTC_EXT="$PREBUILTS_DIR/bin/dtc" \
-        DTC_OVERLAY_TEST_EXT="$PREBUILTS_DIR/bin/ufdt_apply_overlay" \
-        TARGET_PRODUCT=$TARGET $@ || exit $?
-}
+echo_i() { echo -e "\n\033[1;36m==> $1\033[0m\n"; }
 
-function get_trees_rev() {
+echo_w() { echo -e "\033[1;33m $1\033[0m"; }
+
+echo_e() { echo -e "\n\033[1;31m $1\033[0m\n"; }
+
+get_trees_rev() {
     kernel_rev="$(git rev-parse HEAD | cut -c1-10)"
     [[ -n "$(git --no-optional-locks status -uno --porcelain)" ]] && kernel_rev+="+"
 
@@ -124,149 +115,161 @@ function get_trees_rev() {
     echo "-${kernel_rev}-m${modules_rev}-d${dt_rev}"
 }
 
-$DO_CLEAN && (
-    rm -rf out $MODULES_REPO
-    echo "Cleaned output directories."
-)
+m() {
+    make -j$(nproc --all) O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 \
+        DTC_EXT="$PREBUILTS_DIR/bin/dtc" \
+        DTC_OVERLAY_TEST_EXT="$PREBUILTS_DIR/bin/ufdt_apply_overlay" \
+        TARGET_PRODUCT=$TARGET $@ || exit $?
+}
 
-mkdir -p out
+build_kernel() {
+    echo_i "Building kernel image..."
+    m Image
+    cp out/arch/arm64/boot/Image $KERNEL_COPY_TO
+    echo_i "Copied kernel to $KERNEL_COPY_TO."
+}
+
+build_modules() {
+    echo_i "Building kernel modules..."
+    m modules
+    rm -rf out/modules out/*.ko
+    m INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install
+
+    ksu_path="$(find $modules_out -name 'kernelsu.ko' -print -quit)"
+    if [ -n "$ksu_path" ]; then
+        mv "$ksu_path" out
+        echo_i "Copied to out/kernelsu.ko"
+    else
+        echo_e "Unable to locate ksu module!"
+    fi
+
+    echo_i "Building techpack modules..."
+    for module in $MODULES; do
+        echo -e "\nBuilding $module..."
+        m -C $MODULES_SRC/$module M=$MODULES_SRC/$module KERNEL_SRC="$(pwd)" OUT_DIR="$(pwd)/out"
+        m -C $MODULES_SRC/$module M=$MODULES_SRC/$module KERNEL_SRC="$(pwd)" OUT_DIR="$(pwd)/out" \
+            INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install
+    done
+
+    first_stage_modules="$(cat modules.list.msm.waipio)"
+    second_stage_modules="$(cat modules.list.second_stage modules.list.second_stage.$TARGET)"
+    vendor_dlkm_modules="$(cat modules.list.vendor_dlkm modules.list.vendor_dlkm.$TARGET)"
+    modules_out="out/modules/lib/modules/$(ls -t out/modules/lib/modules/ | head -n1)"
+
+    rm -rf $VBOOT_DIR && mkdir -p $VBOOT_DIR
+    rm -rf $VDLKM_DIR && mkdir -p $VDLKM_DIR
+
+    echo_i "Copying first stage modules..."
+    for module in $first_stage_modules; do
+        mod_path=$(find $modules_out -name "$module" -print -quit)
+        if [ -z "$mod_path" ]; then
+            echo_w "Could not locate $module, skipping!"
+            continue
+        fi
+        cp $mod_path $VBOOT_DIR
+        echo $module >> $VBOOT_DIR/modules.load
+        echo $module >> $VBOOT_DIR/modules.load.recovery
+        echo $module
+    done
+
+    echo_i "Copying second stage modules..."
+    for module in $second_stage_modules; do
+        mod_path=$(find $modules_out -name "$module" -print -quit)
+        if [ -z "$mod_path" ]; then
+            echo_w "Could not locate $module, skipping!"
+            continue
+        fi
+        cp $mod_path $VBOOT_DIR
+        cp $mod_path $VDLKM_DIR
+        echo $module >> $VBOOT_DIR/modules.load.recovery
+        echo $module >> $VDLKM_DIR/modules.load
+        echo $module
+    done
+
+    echo_i "Copying vendor_dlkm modules..."
+    for module in $vendor_dlkm_modules; do
+        mod_path=$(find $modules_out -name "$module" -print -quit)
+        if [ -z "$mod_path" ]; then
+            echo_w "Could not locate $module, skipping!"
+            continue
+        fi
+        cp $mod_path $VDLKM_DIR
+        echo $module >> $VDLKM_DIR/modules.load
+        echo $module
+    done
+
+    for dest_dir in $VBOOT_DIR $VDLKM_DIR; do
+        cp modules.vendor_blocklist.msm.waipio $dest_dir/modules.blocklist
+        cp $modules_out/modules.{alias,dep,softdep} $dest_dir
+    done
+
+    sed -E -i 's|([^: ]*/)([^/]*\.ko)([:]?)([ ]\|$)|/lib/modules/\2\3\4|g' $VBOOT_DIR/modules.dep
+    sed -E -i 's|([^: ]*/)([^/]*\.ko)([:]?)([ ]\|$)|/vendor_dlkm/lib/modules/\2\3\4|g' $VDLKM_DIR/modules.dep
+}
+
+build_dtbs() {
+    echo_i "Building dtbs..."
+    m dtbs
+
+    rm -rf out/dtbs{,-base}
+    mkdir out/dtbs{,-base}
+    mv  out/arch/arm64/boot/dts/vendor/qcom/$DTB_WILDCARD.dtb \
+        out/arch/arm64/boot/dts/vendor/qcom/$DTBO_WILDCARD.dtbo \
+        out/dtbs-base
+    rm -f out/arch/arm64/boot/dts/vendor/qcom/*.dtbo
+    ../../build/android/merge_dtbs.py out/dtbs-base out/arch/arm64/boot/dts/vendor/qcom/ out/dtbs || exit $?
+
+    if [ -d "$DTB_COPY_TO" ]; then
+        rm -f $DTB_COPY_TO/*.dtb
+        cp out/dtbs/*.dtb $DTB_COPY_TO
+    else
+        rm -f $DTB_COPY_TO
+        cat out/dtbs/*.dtb >> $DTB_COPY_TO
+    fi
+    echo_i "Copied dtb(s) to $DTB_COPY_TO."
+
+    mkdtboimg.py create $DTBO_COPY_TO --page_size=4096 out/dtbs/*.dtbo
+    echo_i "Generated dtbo.img to $DTBO_COPY_TO".
+}
+
+##
+## Main logic starts here
+##
+
+export PATH="$TC_DIR/bin:$PREBUILTS_DIR/bin:$PATH"
 export LOCALVERSION="$(get_trees_rev)"
 
-echo -e "Generating config...\n"
+$DO_CLEAN && {
+    rm -rf out $MODULES_REPO
+    echo_i "Cleaned output directories."
+}
+
+mkdir -p out
+
+echo_i "Generating config..."
 m $DEFCONFIG
 m ./scripts/kconfig/merge_config.sh $DEFCONFIGS vendor/${TARGET}_GKI.config
 scripts/config --file out/.config \
     --set-str LOCALVERSION "-$BRANCH" \
     -d LOCALVERSION_AUTO \
     -m CONFIG_KSU
-$NO_LTO && (
+$NO_LTO && {
     scripts/config --file out/.config \
         --set-str LOCALVERSION "-${BRANCH}-nolto" \
         -d LTO_CLANG_FULL -e LTO_NONE
-    echo -e "\nDisabled LTO!"
-)
+    echo_i "Disabled LTO!"
+}
 
 $ONLY_CONFIG && exit
 
-echo -e "\nBuilding kernel...\n"
-m Image modules dtbs
-rm -rf out/modules out/*.ko
-m INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install
+if $ONLY_KERNEL; then build_kernel
+elif $ONLY_DTB; then build_dtbs
+elif $ONLY_MODULES; then build_modules
+else {
+    build_kernel
+    build_modules
+    build_dtbs
+}; fi
 
-echo -e "\nCopying KSU LKM..."
-ksu_path="$(find $modules_out -name 'kernelsu.ko' -print -quit)"
-if [ -n "$ksu_path" ]; then
-    mv "$ksu_path" out
-    echo "Copied to out/kernelsu.ko"
-else
-    echo "Unable to locate ksu module!"
-fi
 
-echo -e "\nBuilding techpack modules..."
-for module in $MODULES; do
-    echo -e "\nBuilding $module..."
-    m -C $MODULES_SRC/$module M=$MODULES_SRC/$module KERNEL_SRC="$(pwd)" OUT_DIR="$(pwd)/out"
-    m -C $MODULES_SRC/$module M=$MODULES_SRC/$module KERNEL_SRC="$(pwd)" OUT_DIR="$(pwd)/out" \
-        INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install
-done
-
-echo -e "\nKernel compiled succesfully!\nMerging dtb's...\n"
-
-rm -rf out/dtbs{,-base}
-mkdir out/dtbs{,-base}
-mv  out/arch/arm64/boot/dts/vendor/qcom/$DTB_WILDCARD.dtb \
-    out/arch/arm64/boot/dts/vendor/qcom/$DTBO_WILDCARD.dtbo \
-    out/dtbs-base
-rm -f out/arch/arm64/boot/dts/vendor/qcom/*.dtbo
-../../build/android/merge_dtbs.py out/dtbs-base out/arch/arm64/boot/dts/vendor/qcom/ out/dtbs || exit $?
-
-echo -e "\nCopying files...\n"
-
-# rm -rf AnyKernel3
-# if [ -d "$AK3_DIR" ]; then
-# 	cp -r $AK3_DIR AnyKernel3
-# 	git -C AnyKernel3 checkout marble &> /dev/null
-# elif ! git clone -q https://github.com/ghostrider-reborn/AnyKernel3 -b marble; then
-# 	echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
-# 	exit 1
-# fi
-# KERNEL_COPY_TO="AnyKernel3"
-# DTB_COPY_TO="AnyKernel3/dtb"
-# DTBO_COPY_TO="AnyKernel3/dtbo.img"
-# VBOOT_DIR="AnyKernel3/vendor_boot_modules"
-# VDLKM_DIR="AnyKernel3/vendor_dlkm_modules"
-
-cp out/arch/arm64/boot/Image $KERNEL_COPY_TO
-echo "Copied kernel to $KERNEL_COPY_TO."
-
-if [ -d "$DTB_COPY_TO" ]; then
-    rm -f $DTB_COPY_TO/*.dtb
-    cp out/dtbs/*.dtb $DTB_COPY_TO
-else
-    rm -f $DTB_COPY_TO
-    cat out/dtbs/*.dtb >> $DTB_COPY_TO
-fi
-echo "Copied dtb(s) to $DTB_COPY_TO."
-
-mkdtboimg.py create $DTBO_COPY_TO --page_size=4096 out/dtbs/*.dtbo
-echo "Generated dtbo.img to $DTBO_COPY_TO".
-
-first_stage_modules="$(cat modules.list.msm.waipio)"
-second_stage_modules="$(cat modules.list.second_stage modules.list.second_stage.$TARGET)"
-vendor_dlkm_modules="$(cat modules.list.vendor_dlkm modules.list.vendor_dlkm.$TARGET)"
-modules_out="out/modules/lib/modules/$(ls -t out/modules/lib/modules/ | head -n1)"
-
-rm -rf $VBOOT_DIR && mkdir -p $VBOOT_DIR
-rm -rf $VDLKM_DIR && mkdir -p $VDLKM_DIR
-
-echo -e "\nCopying first stage modules..."
-for module in $first_stage_modules; do
-    mod_path=$(find $modules_out -name "$module" -print -quit)
-    if [ -z "$mod_path" ]; then
-        echo "Could not locate $module, skipping!"
-        continue
-    fi
-    cp $mod_path $VBOOT_DIR
-    echo $module >> $VBOOT_DIR/modules.load
-    echo $module >> $VBOOT_DIR/modules.load.recovery
-done
-
-echo -e "\nCopying second stage modules..."
-for module in $second_stage_modules; do
-    mod_path=$(find $modules_out -name "$module" -print -quit)
-    if [ -z "$mod_path" ]; then
-        echo "Could not locate $module, skipping!"
-        continue
-    fi
-    cp $mod_path $VBOOT_DIR
-    cp $mod_path $VDLKM_DIR
-    echo $module >> $VBOOT_DIR/modules.load.recovery
-    echo $module >> $VDLKM_DIR/modules.load
-done
-
-echo -e "\nCopying vendor_dlkm modules..."
-for module in $vendor_dlkm_modules; do
-    mod_path=$(find $modules_out -name "$module" -print -quit)
-    if [ -z "$mod_path" ]; then
-        echo "Could not locate $module, skipping!"
-        continue
-    fi
-    cp $mod_path $VDLKM_DIR
-    echo $module >> $VDLKM_DIR/modules.load
-done
-
-for dest_dir in $VBOOT_DIR $VDLKM_DIR; do
-    cp modules.vendor_blocklist.msm.waipio $dest_dir/modules.blocklist
-    cp $modules_out/modules.{alias,dep,softdep} $dest_dir
-done
-
-sed -E -i 's|([^: ]*/)([^/]*\.ko)([:]?)([ ]\|$)|/lib/modules/\2\3\4|g' $VBOOT_DIR/modules.dep
-sed -E -i 's|([^: ]*/)([^/]*\.ko)([:]?)([ ]\|$)|/vendor_dlkm/lib/modules/\2\3\4|g' $VDLKM_DIR/modules.dep
-
-# cd AnyKernel3
-# zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
-# cd ..
-# rm -rf AnyKernel3
-
-echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-# echo "$(realpath $ZIPNAME)"
+echo_i "Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
